37ea4eb688cc015e114bb2a1f2a73dbfa2e6ce78
Eloiza <eloizaeunice25@gmail.com>
2024-11-13 17:00:06 -0400

    alteracao no nome referencia das colunas

--- app.py
+++ app.py
@@ -12,10 +12,20 @@ email = st.text_input('Email')
 senha = st.text_input('Senha', type='password')
 if st.button('Login'):
     usuarios = carregar_usuarios()
-    usuario = usuarios[usuarios['Email'] == email]
+    usuario = usuarios[usuarios['EMAIL'] == email]
     if not usuario.empty:
         st.session_state['usuario'] = usuario.iloc[0].to_dict()
         st.success('Login realizado com sucesso!')
-        st.experimental_rerun()
+        #st.experimental_rerun()
     else:
         st.error('Email ou senha inválidos.')
+if 'usuario' in st.session_state:
+    usuario = st.session_state['usuario']
+    st.title(f'Bem-vindo, {usuario["NOME"]}!')
+    st.subheader('Dashboards Disponíveis')
+    dashboards = usuario['LINKS DA DASHBOARD'].split(';')
+    for dashboard in dashboards:
+        st.markdown(f"- [LINKS DA DASHBOARD]({dashboard})")
+@st.cache_data
+def carregar_usuarios():
+    return pd.read_excel('usuarios_acessos.xlsx', engine='openpyxl')
